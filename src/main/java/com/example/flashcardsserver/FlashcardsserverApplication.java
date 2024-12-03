4cad9e450ae5878b4727bfeb73022f9838dfeff9
Matija Marinković <Matija Marinkovi>
2024-12-03 08:13:51 +0100

    Inicirana Spring Boot Aplikacija

---
+++ src/main/java/com/example/flashcardsserver/FlashcardsserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.flashcardsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardsserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardsserverApplication.class, args);
+	}
+
+}
