2f8f3b627c13cfd7dd6c7cdd64718e3cc97bc777
lekk1z <2021.09.02@online.devetagimnazija.edu.rs>
2024-12-05 12:44:34 +0100

    Grupa kartica

---
+++ src/main/java/com/example/flashcardsserver/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.flashcardsserver.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("")
+    public String homePage() {
+        return "Hello World!";
+    }
+}
