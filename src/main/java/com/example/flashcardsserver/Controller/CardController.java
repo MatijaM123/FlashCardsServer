02912ad22f5efabf0742611363ed70c16a0c2069
Matija Marinković <bombmatija@gmail.com>
2024-12-05 21:38:05 +0100

    Dodata PUT metoda za Flashcard.

--- src/main/java/com/example/flashcardsserver/Controller/CardController.java
+++ src/main/java/com/example/flashcardsserver/Controller/CardController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,4 +47,9 @@ public class CardController {
         return ResponseEntity.ok(cards);
     }
 
+    @PutMapping("/")
+    public ResponseEntity<Card> updateCard(@RequestBody Card card){
+       Card nc =  cardService.updateCard(card.getId(),card);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(nc);
+    }
 }
